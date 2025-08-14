87c7b3fefb97466259d489c1c64522089a5ae5f4
Tamim Islam <nottamimislam@gmail.com>
2025-08-15 02:38:50 +0600

    jj

--- event_project/settings.py
+++ event_project/settings.py
@@ -105,10 +105,13 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
